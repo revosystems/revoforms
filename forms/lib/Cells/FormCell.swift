2129c7baf035b89ea194df76b0703a7908cf60c8
Jordi Puigdellívol <jordi.p@revo.works>
2022-01-10 10:41:16 +0100

    Form cell coder

--- forms/lib/Cells/FormCell.swift
+++ forms/lib/Cells/FormCell.swift
@@ -12,7 +12,7 @@ public class FormCell : UITableViewCell {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     // MARK:- Setup
