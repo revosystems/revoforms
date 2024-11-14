ae79c8b20ecf8ec9174963a5fc5658c7fbf702ea
Sergi Baró <sergi.baro@revo.works>
2024-11-14 15:45:55 +0100

    minor change

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             name: "RevoForms",
             type: .static,
             targets: ["RevoForms"]
-        ),
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/revosystems/RevoValidation.git", .upToNextMinor(from: "0.1.0"))
