ff90efafa29deecd3769e65da53f655e3dec1b2a
Sergi Baró <sergi.baro@revo.works>
2024-11-14 15:41:47 +0100

    added Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "RevoForms",
+    platforms: [
+        .iOS(.v11_3)
+    ],
+    products: [
+        .library(
+            name: "RevoForms",
+            type: .static,
+            targets: ["RevoForms"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/revosystems/RevoValidation.git", .upToNextMinor(from: "0.1.0"))
+    ],
+    targets: [
+        .target(
+            name: "RevoForms",
+            dependencies: ["RevoValidation"],
+            path: "forms/lib"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
